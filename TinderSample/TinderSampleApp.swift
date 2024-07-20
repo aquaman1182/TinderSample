175ec42fac2f90ce1c89e29d679178f401999daa
aquaman1182 <hal.bandotakumi@gmail.com>
2024-07-20 12:24:25 +0900

    Initial Commit

---
+++ TinderSample/TinderSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSampleApp.swift
+//  TinderSample
+//
+//  Created by 拓実 on 2024/07/20.
+//
+
+import SwiftUI
+
+@main
+struct TinderSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
